e87b6e3429e850219578cfb32a0004bb5af88fb6
Naji <lemiine987@gmail.com>
2023-05-23 16:10:14 +0300

    Add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+
+import {
+  Navbar,
+  Exchanges,
+  HomePage,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex">
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Main */}
+      <div className="flex flex-col flex-1">
+        {/* Routes */}
+        <div>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/exchanges" element={<Exchanges />} />
+            <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+            <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+            <Route path="/news" element={<News />} />
+          </Routes>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,76 @@
+import { Link, useMatch } from "react-router-dom";
+import { AiOutlineHome } from "react-icons/ai";
+import { RiExchangeBoxLine } from "react-icons/ri";
+import { MdOutlineSsidChart } from "react-icons/md";
+import { IoNewspaperOutline } from "react-icons/io5";
+
+export default function Navbar() {
+  const homeMatch = useMatch({ path: "/", exact: true });
+  const cryptocurrenciesMatch = useMatch("/cryptocurrencies");
+  const exchangesMatch = useMatch("/exchanges");
+  const newsMatch = useMatch("/news");
+
+  return (
+    <div className="navbar bg-black flex flex-col h-screen w-1/4 gap-10">
+      <div className="flex items-center justify-center p-2">
+        <div className="w-24 rounded-full">
+          <img
+            src="/images/stock/photo-1534528741775-53994a69daeb.jpg"
+            alt="Avatar"
+          />
+        </div>
+        <Link to="/" className="text-2xl font-bold text-white ml-2">
+          CryptoVerse
+        </Link>
+      </div>
+      <div>
+        <ul className="menu bg-black rounded-box">
+          <li>
+            <Link
+              to="/"
+              className={`text-white text-xl flex items-center justify-center p-2 ${
+                homeMatch ? "text-rose-500" : ""
+              }`}
+            >
+              <AiOutlineHome />
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/cryptocurrencies"
+              className={`text-white text-xl flex items-center justify-center p-2 ${
+                cryptocurrenciesMatch ? "text-rose-500" : ""
+              }`}
+            >
+              <MdOutlineSsidChart />
+              Cryptocurrencies
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/exchanges"
+              className={`text-white text-xl flex items-center justify-center p-2 ${
+                exchangesMatch ? "text-rose-500" : ""
+              }`}
+            >
+              <RiExchangeBoxLine />
+              Exchanges
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/news"
+              className={`text-white text-xl flex items-center justify-center p-2 ${
+                newsMatch ? "text-rose-500" : ""
+              }`}
+            >
+              <IoNewspaperOutline />
+              News
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
